dbe09e45cae2bc100425060d36d2d7386dfce5f4
Flammie A Pirinen <flammie@iki.fi>
2025-12-29 17:02:15 +0100

    initial working version

--- latex2markdown/latex2markdown.py
+++ latex2markdown/latex2markdown.py
@@ -1,10 +1,49 @@
 #!/usr/bin/env python3
 """Simple converter from latex papers to github pages markdowns."""
 
-import sys
+import os
 import re
-
+import sys
 from argparse import ArgumentParser, FileType
+
+
+def readbib(relpath: str, bibfile: str):
+    """Read bibfile into a map of maps."""
+    with open(os.path.join(relpath, bibfile + ".bib")) as f:
+        curkey = None
+        bib = {}
+        for line in f:
+            line = line.strip()
+            if line.startswith("@"):
+                bracket = line.find("{")
+                comma = line.find(",")
+                curkey = line[bracket+1:comma]
+                bib[curkey] = {}
+            elif line.startswith("}"):
+                curkey = None
+            elif "=" in line:
+                if curkey is None:
+                    print(f"error parsing {bibfile} data before key",
+                          file=sys.stderr)
+                    continue
+                fields = line.split("=")
+                thing = fields[0].strip()
+                stuff = fields[1].strip()
+                if stuff.startswith("{") and "}" not in line:
+                    line = next(f)
+                    while "}" not in line:
+                        stuff = stuff + line.strip()
+                        line = next(f)
+                if stuff.startswith("{") or stuff.startswith("\""):
+                    stuff = stuff[1:]
+                if stuff.endswith("}") or stuff.endswith("\""):
+                    stuff = stuff[:-1]
+                elif stuff.endswith("},") or stuff.endswith("\","):
+                    stuff = stuff[:-2]
+                bib[curkey][thing] = stuff.replace("\\", "\\\\")
+            else:
+                pass
+    return bib
 
 
 def main():
@@ -17,25 +56,32 @@ def main():
     ap.add_argument("-v", "--verbose", action="store_true", default=False,
                     help="print verbosely while processing")
     opts = ap.parse_args()
+    relpath = os.getcwd()
     if not opts.infile:
         opts.infile = sys.stdin
         print("reading from <stdin>")
+    else:
+        relpath = os.path.dirname(os.path.realpath(opts.infile.name))
     if not opts.outfile:
         opts.outfile = sys.stdout
     latex = ""
-    # comments
+    # read in and remvoe comments
     for line in opts.infile.readlines():
         if "%" in line:
             line = line.replace("\\%", "§PERCENT§")
             line = line.split("%")[0].replace("§PERCENT§", "%")
         latex = latex + line
+    # get rid of html / markdown problems
+    latex = latex.replace("<", "&lt;")
+    latex = latex.replace(">", "&gt;")
+    # document class
     documentclassre = re.compile(r"\\documentclass(\[[^]]*\])?({[^}]*})")
     documentclasses = documentclassre.findall(latex)
     if len(documentclasses) == 0:
-        print("Coildn't find documentclass maybe not latex")
+        print("Coildn't find documentclass maybe not latex", file=sys.stderr)
         sys.exit(1)
     elif len(documentclasses) > 1:
-        print("Found too many documentclasses")
+        print("Found too many documentclasses", file=sys.stderr)
         sys.exit(1)
     # headings
     documentclass = documentclasses[0][1]
@@ -56,7 +102,8 @@ def main():
     labelcount = 1
     for label in labels:
         if label in labelmap:
-            print(f"Duplicate label {label}! References may fail")
+            print(f"Duplicate label {label}! References may fail",
+                  file=sys.stderr)
         else:
             labelmap[label] = "LABEL " + label
             labelcount = labelcount + 1
@@ -64,34 +111,137 @@ def main():
     refre = re.compile(r"\\ref{([^}]*)}")
     refs = refre.findall(latex)
     for ref in refs:
-        if not ref in labelmap:
-            print(f"ref to missing label {ref}, generating borken links")
-    latex = refre.sub("[REF \\1](#\\1)", latex)
+        if ref not in labelmap:
+            print(f"ref to missing label {ref}, generating borken links",
+                  file=sys.stderr)
+    latex = refre.sub("[(see: \\1)](#\\1)", latex)
+    # bibliographies... absolute first fist
+    bibliographyre = re.compile(r"\\bibliography{([^}]*)}")
+    bibs = bibliographyre.findall(latex)
+    bibmap = {}
+    for bib in bibs:
+        bibmap.update(readbib(relpath, bib))
     citere = re.compile(r"\\cite{([^}]*)}")
     cites = citere.findall(latex)
-    # TODO: maybe check against bibs?
-    latex = citere.sub("[CITE \\1](#\\1)", latex)
+    usedbibs = {}
+    for citestr in cites:
+        for cite in citestr.split(","):
+            if cite in bibmap:
+                usedbibs[cite] = bibmap[cite]
+            else:
+                print(f"bib data for {cite} missing, generating broken "
+                      "citation", file=sys.stderr)
+                usedbibs[cite] = {"error": "missing from bibliography"}
+    latex = citere.sub("[(cites: \\1)](#\\1)", latex)
+    bibcontent = "# References\n\n"
+    # no support for bibliography styles, we just dump all available data
+    bibliographystylere = re.compile(r"\\bibliographystyle{([^}]*)}")
+    latex = bibliographystylere.sub(r"<!-- bib style: \1 -->", latex)
+    for key, bib in usedbibs.items():
+        bibcontent += f"* <a id=\"{key}\">**{key}**</a>:\n"
+        for k, v in bib.items():
+            if len(v) > 60:
+                v = v[:60] + "..."
+            bibcontent += f"    * {k}: {v}\n"
+    latex = bibliographyre.sub(bibcontent, latex)
+    # smallest local things first: chars and codes
+    latex = latex.replace("\\\\",
+                          "<!-- LINEBREAK -->")  # should be linebreak but but
+    latex = latex.replace("``", "“")
+    latex = latex.replace("''", "”")
+    latex = latex.replace("`", "‘")
+    latex = latex.replace("'", "’")
+    latex = latex.replace("~", " ")
+    latex = latex.replace("\\ ", " ")   # FIXME: other space?
+    latex = latex.replace("\\\"{o}", "ö")
+    # tabulars...
+    tabularre = re.compile(r"\\begin{tabular}.*?\\end{tabular}",
+                           re.MULTILINE | re.DOTALL)
+    tabulars = tabularre.findall(latex)
+    tablecontent = ""
+    for tabular in tabulars:
+        tablecontent = tabular
+        tablecontent = tablecontent.replace(r"\begin{tabular}", "")
+        tablecontent = tablecontent.replace(r"\end{tabular}", "")
+        if tablecontent.startswith("["):
+            tablecontent = tablecontent[tablecontent.find("]", 1)+1:]
+        columns = 0
+        if tablecontent.startswith("{"):
+            columnstr = tablecontent[1:tablecontent.find("}", 1)]
+            columns = columns + columnstr.count("l")
+            columns = columns + columnstr.count("r")
+            columns = columns + columnstr.count("c")
+            columns = columns + columnstr.count("p")
+            tablecontent = tablecontent[tablecontent.find("}", 1)+1:]
+        linerule = "+"
+        linerule = linerule + ("----+" * columns)
+        tablecontent = linerule + tablecontent
+        tablefinal = ""
+        for line in tablecontent.split("\n"):
+            if "&" in line:
+                line = "| " + line.replace("&", " | ")
+            if "<!-- LINEBREAK -->" in line:
+                line = line.replace("<!-- LINEBREAK -->",
+                                    "|\n" + linerule + "\n")
+            line = line.replace("\\toprule", "<!-- toprule -->")
+            line = line.replace("\\midrule", "<!-- midrule -->")
+            line = line.replace("\\bottomrule", "<!-- bottomrule -->")
+            line = line.replace("\\hline", "<!-- hline -->")
+            tablefinal += line + "\n"
+        latex = latex.replace(tabular, tablefinal)
+
     # small local things first
+    mathre = re.compile(r"\$([^$]*)\$")
+    latex = mathre.sub(r"<span class='math'>\1</span>", latex)
     textttre = re.compile(r"\\texttt{([^}]*)}", re.MULTILINE)
     latex = textttre.sub(r"`\1`", latex)
     textbfre = re.compile(r"\\textbf{([^}]*)}", re.MULTILINE)
     latex = textbfre.sub(r"**\1**", latex)
     textitre = re.compile(r"\\textit{([^}]*)}", re.MULTILINE)
     latex = textitre.sub(r"*\1*", latex)
+    textscre = re.compile(r"\\textsc{([^}]*)}", re.MULTILINE)
+    latex = textscre.sub(r"<span style='font-variant: small-caps'>\1</span>",
+                         latex)
     urlre = re.compile(r"\\url{([^}]*)}", re.MULTILINE)
     latex = urlre.sub(r"<\1>", latex)
     footnotere = re.compile(r"\\footnote{([^}]*)}", re.MULTILINE)
     latex = footnotere.sub(r" (footnote: \1)", latex)
     textcolorre = re.compile(r"\\textcolor{([^}]*)}{([^}]*)}", re.MULTILINE)
     latex = textcolorre.sub(r"<span style='color: \1'>\2</span>", latex)
+    latex = latex.replace("\\appendix", "* * *\n\n# Appendix\n")
+    captionre = re.compile(r"\\caption{([^}]*)}", re.MULTILINE)
+    latex = captionre.sub(r" (Caption: \1)", latex)
+    # flammie specific
+    aappdre = re.compile(r"\\aclanthologypostprintdoi{([^}]*)}")
+    latex = aappdre.sub("Publisher’s version available at [ACL Anthology "
+                        r"identifier: \1](https://aclanthology.org/\1). "
+                        "All modern "
+                        "ACL conferences are open access usually CC BY",
+                        latex)
+    springerre = re.compile(r"\\springerpostprintdoi{([^}]*)}")
+    latex = springerre.sub("Publisher’s version available at [Springer via "
+                           r"doi: \1](https://dx.doi.org/\1). For more "
+                           "information, see [Springers self archiving "
+                           "policy]"
+                           "(http://www.springer.com/gp/open-access/"
+                           "authors-rights/self-archiving-policy/2124).",
+                           latex)
+    fnprre = re.compile(r"\\footnotepubrights{([^}]*)}", re.MULTILINE)
+    latex = fnprre.sub("¹\n§TITLEFOOTNOTE§"
+                       "<span style='font-size:8pt'>(¹ Authors' archival "
+                       r"version: \1)</span>", latex)
     # includegraphics...
-    #                                 \includegraphics[width=.5\textwidth ]{syntaxflow.png}
+    # \includegraphics[width=.5\textwidth]{syntaxflow.png}
     includegraphicsre = re.compile(r"\\includegraphics(\[[^]]*\])?{([^}]*)}")
     latex = includegraphicsre.sub(r"![\2](\2)", latex)
     # Linguistics
-    latex = latex.replace("\\ex.", "*Linguistic examples*:\n")
+    latex = latex.replace("\\ex.", "*Linguistic examples*:\n\n")
+    latex = latex.replace("\\exg.", "*Linguistic examples g*:\n\n")
+    latex = latex.replace("\\ag.", "a. ")
+    latex = latex.replace("\\b.", "b. ")
     # even more simple stuffs
     latex = latex.replace("\\begin{document}", "<!-- begin document -->")
+    latex = latex.replace("\\end{document}", "<!-- end document -->")
     latex = latex.replace("\\maketitle", "<!-- make title -->")
     latex = latex.replace("\\begin{abstract}", "*Abstract:*")
     latex = latex.replace("\\end{abstract}", "<!-- end abstract -->")
@@ -106,8 +256,7 @@ def main():
     latex = latex.replace("\\begin{verbatim}", "\n```\n")
     latex = latex.replace("\\end{verbatim}", "\n```\n")
     latex = latex.replace("\\centering", "<!-- centering -->")
-    latex = latex.replace("\\and", "and")
-    latex = latex.replace("\\\\", " ")  # should be linebreak but but
+    latex = latex.replace("\\and", "\nand\n\n")
     # stuffs
     sectionre = re.compile(r"\\section{([^}]*)}", re.MULTILINE)
     latex = sectionre.sub(r"## \1", latex)
@@ -127,9 +276,16 @@ def main():
     title = titles[0].replace("\n", " ")
     latex = titlere.sub(r"# §§§TITLE§§§", latex)
     latex = latex.replace("§§§TITLE§§§", title)
+    latex = latex.replace("§TITLEFOOTNOTE§", "\n\n")
     authorre = re.compile(r"\\author{([^}]*)}", re.MULTILINE)
     latex = authorre.sub(r"Authors: \1", latex)
+    # final fixes
+    latex = latex.replace("<!-- LINEBREAK -->", "\n")
     markdown = latex
+    markdown = markdown + "\n* * *\n\n" + \
+        "<span style='font-size: 8pt'>Converted with [Flammie’s " + \
+        "latex2markdown](https://github.com/flammie/latex2markdown) v." + \
+        "0.1.0</span>\n"  # FIXME: get version somewhere
     print(markdown, file=opts.outfile)
 
 
